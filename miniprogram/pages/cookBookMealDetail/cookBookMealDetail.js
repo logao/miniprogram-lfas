2f9c88449f1432f2c50ad138222086dc0ed25f4a
logao <693990003@qq.com>
2020-12-03 14:05:46 +0800

    tmpSave_1203

--- miniprogram/pages/cookBookMealDetail/cookBookMealDetail.js
+++ miniprogram/pages/cookBookMealDetail/cookBookMealDetail.js
@@ -6,11 +6,15 @@ Page({
   data: {
     operation: 'add',
     mealTypeRadio: [
-      { name: "荤", value: 0, checked: true },
-      { name: "素", value: 1 },
-      { name: "主", value: 0 },
+      { name: "荤菜", value: 0, checked: true },
+      { name: "素菜", value: 1 },
+      { name: "主食", value: 0 },
     ],
-    localMealPhotoPath: '/images/icons/upload-meal-pic-default.png'
+    localMealPhotoPath: '/images/icons/upload-meal-pic-default.png',
+    mealDesc: '',
+    mealWeightNum: 5,
+    disabled1: false,
+    disabled2: false
   },
   onLoad: function (options) {
     console.log('onload', (new Date()).valueOf())
@@ -25,6 +29,31 @@ Page({
       }
     })
   },
+
+  inputMealName(e) {
+    this.setData({ mealName: e.detail.value.trim() });
+  },
+  mealWeightNumNextNum(e) {
+    this.data.mealWeightNum = this.data.mealWeightNum < 1 ? 0 : this.data.mealWeightNum - 1 
+    this.setData({ 
+      mealWeightNum: this.data.mealWeightNum,
+      disabled1: this.data.mealWeightNum !== 0 ? false : true,
+      disabled2: this.data.mealWeightNum !== 10 ? false : true
+
+    });
+  },
+  mealWeightNumPreNum(e) {
+    this.data.mealWeightNum = this.data.mealWeightNum > 9 ? 10 : this.data.mealWeightNum + 1,
+    this.setData({
+      mealWeightNum: this.data.mealWeightNum,
+      disabled1: this.data.mealWeightNum !== 0 ? false : true,
+      disabled2: this.data.mealWeightNum !== 10 ? false : true
+    });
+  },
+  inputMealDesc(e) {
+    this.setData({ mealDesc: e.detail.value.trim() });
+  },
+
   formSubmit: async function (event) {
 
     var eventData = event.detail.value
